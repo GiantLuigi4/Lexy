9d3c2fb2e099f1ce4411503b96d9a046d32d3161
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2024-03-18 12:17:57 -0400

    add predicate to WordSetBranch

--- src/main/java/tfc/lexy/builtin/text/WordSetBranch.java
+++ src/main/java/tfc/lexy/builtin/text/WordSetBranch.java
@@ -4,22 +4,33 @@ import tfc.lexy.Branch;
 import tfc.lexy.LexyPosition;
 import tfc.lexy.util.StringReader;
 
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 public class WordSetBranch extends Branch<Character> {
     String[] accepted;
     Function<String, Object> mapper;
+    BiPredicate<LexyPosition<Character>, String> validator;
 
     public WordSetBranch(String[] accepted, Function<String, Object> mapper) {
         this.accepted = accepted;
         this.mapper = mapper;
+        validator = (a, b) -> true;
+    }
+
+    public WordSetBranch(String[] accepted, Function<String, Object> mapper, BiPredicate<LexyPosition<Character>, String> validator) {
+        this.accepted = accepted;
+        this.mapper = mapper;
+        this.validator = validator;
     }
 
     @Override
     public boolean shouldStart(LexyPosition<Character> position) {
         for (String s : accepted) {
-            if (((StringReader) position.provider).startsWith(s))
-                return true;
+            if (((StringReader) position.provider).startsWith(s)) {
+                if (validator.test(position, s))
+                    return true;
+            }
         }
         return false;
     }
